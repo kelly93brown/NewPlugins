f4b478d875b52be96d030e4147ab2e6e4ed61193
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-10 20:40:22 +0100

    Update build.gradle.kts

--- plugins/ExampleProvider/build.gradle.kts
+++ plugins/ExampleProvider/build.gradle.kts
@@ -1,38 +1,55 @@
-dependencies {
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+// v5: Define the plugin's identity.
+// This block tells Gradle that this module is an Android Library
+// and that it uses the CloudStream plugin system. This resolves all
+// the "Unresolved reference" errors.
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    // This is the magic line that enables all CloudStream features
+    id("com.lagradost.cloudstream3.plugin")
 }
 
-// Use an integer for version numbers
-version = 1
-
+// The CloudStream plugin block. Now Gradle understands what this is.
 cloudstream {
-    // All of these properties are optional, you can safely remove any of them.
-
-    description = "Lorem ipsum"
+    // All of these will be resolved now.
     authors = listOf("Cloudburst", "Luna712")
-
-    /**
-    * Status int as one of the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta-only
-    **/
-    status = 1 // Will be 3 if unspecified
-
-    tvTypes = listOf("Movie")
-
-    requiresResources = true
+    versionCode = 1
+    // The language of the provider.
     language = "en"
-
-    // Random CC logo I found
-    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Korduene_Logo.png"
+    // The name of the provider.
+    name = "Example Provider"
+    // The type of content the provider hosts.
+    tvTypes = listOf("Movie")
+    // If the provider requires the user to install a separate app for it to work.
+    requiresResources = true
+    // The icon of the provider.
+    iconUrl = "https://upload.wikimedia.org/wikipedia/commons/2/2f/Kordguene_Logo.png"
 }
 
+// The Android configuration block. Gradle also understands this now.
 android {
+    // The namespace of the provider.
+    namespace = "com.example.exampleprovider"
+    // The minimum SDK version the provider supports.
+    minSdk = 21
+    // The target SDK version the provider targets.
+    compileSdk = 34
+
+    // Don't change this.
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    // Don't change this.
     buildFeatures {
-        buildConfig = true
         viewBinding = true
     }
+}
+
+// The dependencies block. 'implementation' is now a known function.
+dependencies {
+    // Don't change this.
+    implementation(project(":app"))
 }
