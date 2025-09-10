8347ad24e88df3b801fa58dd646baed2a4acb74d
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-10 20:28:47 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
-// v3: Register all your plugin modules here.
-// This tells Gradle that "ExampleProvider" is a sub-project it needs to manage.
+// v4: Re-introducing automatic plugin discovery and centralizing repositories.
+// This is the definitive, scalable solution.
 
 pluginManagement {
     repositories {
@@ -8,16 +8,23 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+// Central repository management. This fixes the build error from v3.
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
+
 rootProject.name = "CloudstreamPlugins"
 
-// Include your plugins here
-include(":ExampleProvider")
-// To add a new plugin, create its folder and add a line like this:
-// include(":MyNewArabicPlugin")
+// Automatic plugin inclusion script.
+// It finds all subdirectories in the 'plugins' folder and includes them.
+file("plugins").listFiles()?.forEach {
+    if (it.isDirectory) {
+        include(":plugins:${it.name}")
+    }
+}
