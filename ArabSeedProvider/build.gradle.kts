7f5084cdf9a39c2dd2c13aed25106b296e2b1843
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-12 17:49:01 +0100

    Create build.gradle.kts

---
+++ ArabSeedProvider/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 4
+
+cloudstream {
+    authors = listOf( "ImZaw" )
+
+    language = "ar"
+	
+    status = 1
+
+    tvTypes = listOf( "TvSeries" , "Movie" )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=arabseed.ink&sz=%size%"
+}
