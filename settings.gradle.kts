23a0303b15262d30422a6660da9e317aac3661fc
adamwolker21 <83883573+adamwolker21@users.noreply.github.com>
2025-09-12 05:31:46 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,1 @@
-// v22: إعادة ضبط الإعدادات إلى القالب الرسمي
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-        maven("https://jitpack.io")
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-rootProject.name = "TestPlugins"
+//
